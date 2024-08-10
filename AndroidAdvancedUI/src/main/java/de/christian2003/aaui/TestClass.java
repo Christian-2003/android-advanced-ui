8af77c294691f6124502aa14d95324fa09cb8831
Christian-2003 <80711177+Christian-2003@users.noreply.github.com>
2024-08-10 15:13:39 +0200

    Initial commit

---
+++ AndroidAdvancedUI/src/main/java/de/christian2003/aaui/TestClass.java
@@ -0,0 +1,11 @@
+package de.christian2003.aaui;
+
+import android.util.Log;
+
+public class TestClass {
+
+    public static void testMethod() {
+        Log.d("AAUI", "This is a library test");
+    }
+
+}
